eb423b35aecfffd1c85af0677d8f0ee94452f9fa
pastith <pastith@localhost>
2008-04-05 17:49:42 +0000

    Initial import.

---
+++ src/defaults/preferences/firestatus.js
@@ -0,0 +1,5 @@
+pref("extensions.firestatus.boolpref", false);
+pref("extensions.firestatus.intpref", 0);
+pref("extensions.firestatus.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.firestatus@astithas.com.description", "chrome://firestatus/locale/firestatus.properties");
