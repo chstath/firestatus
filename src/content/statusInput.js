632393199e779054b7cf5372fdd684b46b393c00
chstath <chstath@localhost>
2008-05-21 21:17:18 +0000

    --Converted statusInput into an object --statusInput initialized the services checkboxes according to their last checked state

--- src/content/statusInput.js
+++ src/content/statusInput.js
@@ -15,125 +15,156 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  * */
-
-var firestatus = window.opener.firestatus;
-
+var statusInput = {
+	firestatus : window.opener.firestatus,
 	
-function sendStatusUpdate() {
-	var statusText = document.getElementById('statusText').value;
-	if (firestatus.twitterEnabled && document.getElementById("selectedConsumerTwitter").checked) {
-		sendStatusUpdateTwitter();
-	}
-//	sendStatusUpdateLinkedIn();
-	if (document.getElementById("selectedConsumerFacebook").checked) {
-		sendStatusUpdateFacebook();
-	}
-	//sendStatusUpdateFriendFeed();
-}
-
-function sendStatusUpdateTwitter() {
-	var statusText = document.getElementById('statusText').value +" "+ getShrinkedUrl();
-    var status = encodeURIComponent(statusText);
-    var req = new XMLHttpRequest ();   
-    req.open("POST","http://twitter.com:80/statuses/update.json?status="+status, true);
-    req.onreadystatechange = function () {
-//		firestatus.cons.logStringMessage("twitter readyState: "+req.readyState);
-//		firestatus.cons.logStringMessage("twitter status: "+req.status);
-//		firestatus.cons.logStringMessage("Twitter response: "+req.responseText);
-		if (req.readyState == 4) {
-		     switch(req.status) {
-			 	case 200:
-				 	firestatus.cons.logStringMessage("Twitter update sent.");
-					break;
-				case 400:
-					firestatus.cons.logStringMessage("Bad Request");
-					break;
-				case 401:
-					firestatus.cons.logStringMessage("Not Authorized");
-					break;
-				case 403:
-					firestatus.cons.logStringMessage("Forbidden");
-					break;
-				case 404:
-					firestatus.cons.logStringMessage("Not Found");
-					break;
-				case 500:
-					firestatus.cons.logStringMessage("Internal Server Error");
-					break;
-				case 502:
-					firestatus.cons.logStringMessage("Bad Gateway");
-					break;
-				case 503:
-					firestatus.cons.logStringMessage("Service Unavailable");
-					break;
-				default:
-					firestatus.cons.logStringMessage("Unknown twitter status: "+req.status);
-					firestatus.cons.logStringMessage("Twitter response: "+req.responseText);
-			 }
+	onLoad: function(event) {
+		if (this.firestatus.twitterEnabled) {
+			if (this.firestatus.prefs.prefHasUserValue("lastTwitterChecked")) {
+				document.getElementById("selectedConsumerTwitter").checked = this.firestatus.prefs.getBoolPref("lastTwitterChecked");
+			}
+			else {
+				document.getElementById("selectedConsumerTwitter").checked = true;
+				this.firestatus.prefs.setBoolPref("lastTwitterChecked", true);
+			}
 		}
-	};
-    var auth = firestatus.twitterUsername+":"+firestatus.twitterPassword;
-    req.setRequestHeader("Authorization", "Basic "+btoa(auth));
-    req.send(null); 
-}
+		else {
+			document.getElementById("selectedConsumerTwitter").disabled = true;
+			document.getElementById("selectedConsumerTwitter").checked = false;
+			this.firestatus.prefs.setBoolPref("lastTwitterChecked", false);
+		}
+		if (this.firestatus.prefs.prefHasUserValue("lastFacebookChecked")) {
+			document.getElementById("selectedConsumerFacebook").checked = this.firestatus.prefs.getBoolPref("lastFacebookChecked");
+		}
+		else {
+			document.getElementById("selectedConsumerFacebook").checked = true;
+			this.firestatus.prefs.setBoolPref("lastFacebookChecked", true);
+		}
+	},
+	
+	onUnload: function() {
+		this.firestatus.prefs.setBoolPref("lastTwitterChecked", document.getElementById("selectedConsumerTwitter").checked);
+		this.firestatus.prefs.setBoolPref("lastFacebookChecked", document.getElementById("selectedConsumerFacebook").checked);
+	},
+	
+	sendStatusUpdate: function() {
+		var statusText = document.getElementById('statusText').value;
+		if (this.firestatus.twitterEnabled && document.getElementById("selectedConsumerTwitter").checked) {
+			this.sendStatusUpdateTwitter();
+		}
+	//	sendStatusUpdateLinkedIn();
+		if (document.getElementById("selectedConsumerFacebook").checked) {
+			this.sendStatusUpdateFacebook();
+		}
+		//sendStatusUpdateFriendFeed();
+	},
 
-function sendStatusUpdateLinkedIn(){
-	return true;
-}
+	sendStatusUpdateTwitter: function () {
+		var statusText = document.getElementById('statusText').value +" "+ getShrinkedUrl();
+	    var status = encodeURIComponent(statusText);
+	    var req = new XMLHttpRequest ();   
+	    req.open("POST","http://twitter.com:80/statuses/update.json?status="+status, true);
+	    req.onreadystatechange = function () {
+	//		firestatus.cons.logStringMessage("twitter readyState: "+req.readyState);
+	//		firestatus.cons.logStringMessage("twitter status: "+req.status);
+	//		firestatus.cons.logStringMessage("Twitter response: "+req.responseText);
+			if (req.readyState == 4) {
+			     switch(req.status) {
+				 	case 200:
+					 	this.firestatus.cons.logStringMessage("Twitter update sent.");
+						break;
+					case 400:
+						this.firestatus.cons.logStringMessage("Bad Request");
+						break;
+					case 401:
+						this.firestatus.cons.logStringMessage("Not Authorized");
+						break;
+					case 403:
+						this.firestatus.cons.logStringMessage("Forbidden");
+						break;
+					case 404:
+						this.firestatus.cons.logStringMessage("Not Found");
+						break;
+					case 500:
+						this.firestatus.cons.logStringMessage("Internal Server Error");
+						break;
+					case 502:
+						this.firestatus.cons.logStringMessage("Bad Gateway");
+						break;
+					case 503:
+						this.firestatus.cons.logStringMessage("Service Unavailable");
+						break;
+					default:
+						this.firestatus.cons.logStringMessage("Unknown twitter status: "+req.status);
+						this.firestatus.cons.logStringMessage("Twitter response: "+req.responseText);
+				 }
+			}
+		};
+	    var auth = this.firestatus.twitterUsername+":"+this.firestatus.twitterPassword;
+	    req.setRequestHeader("Authorization", "Basic "+btoa(auth));
+	    req.send(null); 
+	},
+
+	sendStatusUpdateLinkedIn: function (){
+		return true;
+	},
 
 
-function sendStatusUpdateFacebook(){
-	dump("\n@@@@@@@@@@@@@@@@@@@@@Starting facebook update\n")
-	var statusText = document.getElementById('statusText').value +" "+ document.getElementById('statusTextUrl').value;
-//	var status = encodeURIComponent(statusText); //Somehow the status update fails if the status is encoded
+	sendStatusUpdateFacebook: function (){
+		dump("\n@@@@@@@@@@@@@@@@@@@@@Starting facebook update\n")
+		var statusText = document.getElementById('statusText').value +" "+ document.getElementById('statusTextUrl').value;
+	//	var status = encodeURIComponent(statusText); //Somehow the status update fails if the status is encoded
+	
+		var Cc = Components.classes;
+		var Ci = Components.interfaces;
+		// Load facebook code...
+		Cc['@mozilla.org/moz/jssubscript-loader;1']
+	   		.getService(Ci.mozIJSSubScriptLoader)
+	   		.loadSubScript('chrome://firestatus/content/facebookClient.js'); //Is there any other way to gain access to the facebookClient object ??
+		var session = facebookClient.getSession(); //The session can be stored for subsequent calls to facebook api
+		dump(session.session_key + "\n");
+		dump(session.error_code + "\n");
+		if (session.error_code == undefined) {
+			this.sendFacebook(session, statusText);
+		}
+		else alert("Facebook status will not be updated");
+	},
 
-	var Cc = Components.classes;
-	var Ci = Components.interfaces;
-	// Load facebook code...
-	Cc['@mozilla.org/moz/jssubscript-loader;1']
-   		.getService(Ci.mozIJSSubScriptLoader)
-   		.loadSubScript('chrome://firestatus/content/facebookClient.js'); //Is there any other way to gain access to the facebookClient object ??
-	var session = facebookClient.getSession(); //The session can be stored for subsequent calls to facebook api
-	dump(session.session_key + "\n");
-	dump(session.error_code + "\n");
-	if (session.error_code == undefined) {
-		sendFacebook(session, statusText);
-	}
-	else alert("Facebook status will not be updated");
-}
-
-	function sendFacebook(session, statusText) {
+	sendFacebook: function (session, statusText) {
 		var code = facebookClient.updateStatus(session.session_key, session.secret, statusText);
 		if (code == 250) {
-			window.open("http://www.facebook.com/authorize.php?api_key=" + facebookClient.apiKey + "&v=1.0&ext_perm=status_update", "", "chrome, centerscreen,width=646,height=520,modal=yes,dialog=yes,close=yes");
-			sendFacebook(session, statusText);
+			window.open("http://www.facebook.com/authorize.php?api_key=" + facebookClient.apiKey + "&v=1.0&ext_perm=status_update&popup=", "", "chrome, centerscreen,width=646,height=520,modal=yes,dialog=yes,close=yes");
+			this.sendFacebook(session, statusText);
 		}
 		else if (code == 102) {
 			session = facebookClient.getSession(true);
-			sendFacebook(session, statusText);
+			this.sendFacebook(session, statusText);
 		}
 		else if (code != "")
 			alert("Facebook status will not be updated");
-	}
+	},
 
-function sendStatusUpdateFriendFeed() {
-	/* var statusText = document.getElementById('statusText').value;
-	var status = encodeURIComponent(status);
-	req = new XMLHttpRequest ();   
-	//req.onreadystatechange = getTwitterResponse; 
-	req.open("POST","http://friendfeed.com//api/share?title="+statusText+"&nickname=firestatus&remotekey=rared739windy", true);
-	req.send(null); */
+	sendStatusUpdateFriendFeed: function () {
+		/* var statusText = document.getElementById('statusText').value;
+		var status = encodeURIComponent(status);
+		req = new XMLHttpRequest ();   
+		//req.onreadystatechange = getTwitterResponse; 
+		req.open("POST","http://friendfeed.com//api/share?title="+statusText+"&nickname=firestatus&remotekey=rared739windy", true);
+		req.send(null); */
+	},
+
+	getShrinkedUrl: function () {
+		var tinyurl = "http://tinyurl.com/api-create.php?url="+encodeURI(document.getElementById('statusTextUrl').value);
+	    var req = new XMLHttpRequest();
+		req.open('GET', tinyurl, false); 
+		req.send(null);
+		if(req.status == 200) {
+			return req.responseText;
+		} else {
+			return '';
+		}
+	}
 }
 
-function getShrinkedUrl() {
-	var tinyurl = "http://tinyurl.com/api-create.php?url="+encodeURI(document.getElementById('statusTextUrl').value);
-    var req = new XMLHttpRequest();
-	req.open('GET', tinyurl, false); 
-	req.send(null);
-	if(req.status == 200) {
-		return req.responseText;
-	} else {
-		return '';
-	}
-}
-
+window.addEventListener("load", function(e) { statusInput.onLoad(e); }, false);
+window.addEventListener("unload", function(e) { statusInput.onUnload(e); }, false);
