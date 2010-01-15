d994c530eda515a485094e95e891e9e2d23dc572
Christos <Christos@POE>
2010-01-15 23:42:48 +0200

    Fine tuned fql query for facebook notifications

--- src/content/facebookClient.js
+++ src/content/facebookClient.js
@@ -565,7 +565,7 @@ var facebookClient = {
                                 "where recipient_id=" + session.uid + 
                                 " and is_unread=1 and is_hidden=0 and updated_time>" + firestatus.queue.lastFacebookTimestamp;
 //        queries.stream = "select post_id, updated_time, message, permalink, message, attribution, actor_id from stream where viewer_id=" + session.uid + " and updated_time>" + firestatus.queue.lastFacebookTimestamp + " and is_hidden=0 and source_id in (SELECT target_id FROM connection WHERE source_id=" + session.uid + ")";    
-        queries.user_stream = "select post_id, updated_time, message, permalink, attribution, actor_id, attachment from stream where updated_time>" + firestatus.queue.lastFacebookTimestamp + " and is_hidden=0 and source_id in (SELECT target_id FROM connection WHERE source_id=" + session.uid + ")";    
+        queries.user_stream = "select post_id, updated_time, message, permalink, attribution, actor_id, attachment from stream where updated_time>" + firestatus.queue.lastFacebookTimestamp + " and is_hidden=0 and source_id in (SELECT target_id FROM connection WHERE is_following=1 and source_id=" + session.uid + ")";    
 //        queries.user_stream = "select post_id, updated_time, message, permalink, message, attribution, actor_id, attachment from stream where updated_time>0 and is_hidden=0 and source_id in (SELECT target_id FROM connection WHERE source_id=" + session.uid + ")";    
         queries.users = "select id, name, pic from profile where id in (select actor_id from #user_stream)";
         var queriesStr = nativeJSON.encode(queries);    
