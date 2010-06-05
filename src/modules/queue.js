dbbaf79c19f200893453a4f3641ce860eaca9f1d
Christos V Stathis (chstath <Christos V Stathis (chstath@gmail.com)>
2010-06-05 17:55:00 +0300

    Changed url for getting updates from twitter to the new one Remove lastTwitterTimestamp from everywhere as it is not used

--- src/modules/queue.js
+++ src/modules/queue.js
@@ -22,7 +22,6 @@ var queue = {
     // A FIFO queue that contains pending notifications.
     updateQueue: [],
     lastTwitterId: 0,
-    lastTwitterTimestamp: "0",
     lastFriendfeedId: 0,
     lastDeliciousTimestamp: "0",
     lastIdenticaId: 0,
